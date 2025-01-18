7c8b220b2ac27220290163369877c74e0b3574a1
Lucas Valenca1 <133028118+lucasvalenca1@users.noreply.github.com>
2025-01-18 09:06:06 +0000

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer>
+  <p>&copy; 2025 Home Essentials. All rights reserved. Proudly based in the UK.</p>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,4 @@
+<header>
+  <h1>Home Essentials</h1>
+  <p>Your one-stop shop for premium kitchenware in the UK</p>
+</header>
